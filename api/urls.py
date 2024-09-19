905d291bd6b606ee3f9d899fd489e4f1f6b0cd31
Emidio Valeretto <emidio.valereto@gmail.com>
2024-09-19 19:28:24 +0100

    chore: create a PostSerializer and assign a router path to it

--- api/urls.py
+++ api/urls.py
@@ -1,7 +1,11 @@
-from django.urls import path
-from . import views
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from .views import PostViewSet
 
+
+post_router = DefaultRouter()
+post_router.register(r'posts', PostViewSet)
 
 urlpatterns = [
-    path('', views.api_home),
+    path('', include(post_router.urls)),
 ]
--- api/views.py
+++ api/views.py
@@ -1,7 +1,8 @@
-from django.http import JsonResponse
+from rest_framework.viewsets import ModelViewSet
+from api.serializers import PostSerializer
+from posts.models import Post
 
 
-def api_home(request, *args, **kwargs):
-    return JsonResponse({
-        'message': 'Hello, World!!'
-    })
+class PostViewSet(ModelViewSet):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
