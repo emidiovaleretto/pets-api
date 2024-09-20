8333d951028285267643d5c3798a590178639913
Emidio Valeretto <emidio.valereto@gmail.com>
2024-09-20 19:42:56 +0100

    chore: set DATABASE_URL

--- core/settings.py
+++ core/settings.py
@@ -14,6 +14,7 @@ import os
 from pathlib import Path
 
 import django_heroku
+import dj_database_url
 from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -131,10 +132,7 @@ WSGI_APPLICATION = 'core.wsgi.application'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.parse(config("DATABASE_URL"))
 }
 
 
